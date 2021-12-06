f7ed4178269f21d8208bf0eaf6851c16f7b1d0a7
LucasL72 <lucasj.ledoux@gmail.com>
2021-12-06 16:52:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,129 @@
+const app = document.getElementById("app")
+const url = "";
+console.log('Check form')
+
+// Déclaration de constante
+const form = document.getElementById('form')
+const input = document.getElementsByTagName('input');
+
+// Input Radio
+for (i = 0; i < input.length; i++) {
+    input[i].onclick = function (e) {
+        if (e.ctrlKey) this.checked = false;
+    }
+}
+
+function makeSearch() {
+
+    // On crée le container
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    // On crée le row 
+    const row = document.createElement("div");
+    row.setAttribute("class", "row")
+    row.setAttribute("id", "listCard")
+
+    // On crée l'Input ( div + span)
+    const divInput = document.createElement("div");
+    divInput.setAttribute("class", "input-group flex-nowrap py-2");
+
+    const spanInput = document.createElement("span");
+    spanInput.setAttribute("class", "input-group-text");
+    spanInput.innerText = "Recherche";
+
+    const input = document.createElement("input");
+    input.setAttribute("class", "form-control");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Votre recherche");
+    input.setAttribute("aria-label", "Votre recherche");
+    input.setAttribute("aria-describedby", "addon-wrapping");
+    input.setAttribute("onkeypress", "");
+    input.setAttribute("id", "inputSearch");
+
+    // On rattache notre container au site et l'input à notre container
+    app.appendChild(container);
+    container.appendChild(divInput);
+    divInput.appendChild(spanInput);
+    divInput.appendChild(input);
+}
+
+
+function maListDeCards() {
+    console.log("maListDeCards");
+
+    // On crée le container
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    // On crée le row 
+    const row = document.createElement("div");
+    row.setAttribute("class", "row")
+    row.setAttribute("id", "listCard")
+
+    app.appendChild(container);
+    container.appendChild(row);
+
+    const secttitle = document.createElement("h2")
+    secttitle.setAttribute("class", "text-center")
+    secttitle.innerText = "Cards"
+
+    row.appendChild(secttitle);
+
+    // On crée notre boucle 
+    //for (let i = 0; i < arrayImg.length; i++) {
+    const colmd4 = document.createElement("div")
+    colmd4.setAttribute("class", "col-md-4")
+
+    // On construit nos cards + données API
+    const card = document.createElement("div");
+    card.setAttribute("class", "card")
+    card.setAttribute("style", "width:18rem")
+
+    const img = document.createElement("img");
+    img.setAttribute("class", "card-img-top")
+    img.setAttribute("src", "https://pixabay.com/get/gad5eb4328604440b474def3e876e41ec2550cb6bb812994bceef8f5febf427e5a584290ad6a2ffd79a57d6dea69087b3_640.png");
+
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body")
+
+    const title = document.createElement("h5");
+    title.setAttribute("class", "card-title")
+    title.innerText = "type: "
+
+
+
+    const tags = document.createElement("p");
+    tags.setAttribute("class", "card-text");
+    tags.innerText = "tags: "
+
+    const link = document.createElement("a");
+    link.setAttribute("class", "card-text btn btn-primary");
+    link.setAttribute("href", "#");
+    link.setAttribute("target", "_blank");
+    link.innerText = "Voir Photos";
+
+    // ON rattache card à notre row
+    row.appendChild(colmd4);
+    colmd4.appendChild(card);
+    // On rattache img et le cardBody à notre card
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    // on constitue notre body
+    cardBody.appendChild(title);
+    cardBody.appendChild(tags);
+    cardBody.appendChild(link);
+
+
+
+    //}
+}
+
+
+
+function mounted() {
+    makeSearch()
+    maListDeCards()
+
+}
+mounted()
---
+++ myform.js
@@ -0,0 +1,175 @@
+
+
+
+console.log('Mon Script')
+
+function monForm() {
+    // On crée le container
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    // On crée le row 
+    const row = document.createElement("div");
+    row.setAttribute("class", "row")
+    row.setAttribute("id", "listCard")
+
+    app.appendChild(container);
+    container.appendChild(row);
+
+    const titleform = document.createElement("h2")
+    titleform.setAttribute("class", "text-center")
+    titleform.innerText = "Forms"
+
+    row.appendChild(titleform)
+
+    const form = document.createElement("form")
+    form.setAttribute("id", "form")
+    form.setAttribute("style", "display: flex; flex-direction: column;")
+    form.setAttribute("enctype", "application/x-www-form-urlencoded")
+
+    row.appendChild(form)
+
+    const textformlab = document.createElement("label");
+    textformlab.setAttribute("for", "text")
+    textformlab.innerText = "Text";
+    const textforminput = document.createElement("input")
+    textforminput.setAttribute("type", "text")
+    textforminput.setAttribute("name", "text")
+
+
+    form.appendChild(textformlab);
+    textformlab.appendChild(textforminput)
+
+    const textarealab = document.createElement("label")
+    textarealab.setAttribute("for", "textarea")
+    textarealab.innerText = "textarea"
+
+    const textarea = document.createElement("textarea")
+    textarea.setAttribute("name", "textarea")
+    textarea.setAttribute("cols", "30")
+    textarea.setAttribute("rows", "10")
+
+    form.appendChild(textarealab)
+    form.appendChild(textarea)
+
+    const colorlabel = document.createElement("label")
+    colorlabel.setAttribute("for", "color")
+    colorlabel.innerText = "Couleur"
+
+    const colorinput = document.createElement("input")
+    colorinput.setAttribute("type", "color")
+    colorinput.setAttribute("name", "color")
+
+    form.appendChild(colorlabel)
+    form.appendChild(colorinput)
+
+    const datelabel = document.createElement("label")
+    datelabel.setAttribute("for", "date")
+
+    const dateinput = document.createElement("input")
+    dateinput.setAttribute("type", "date")
+    dateinput.setAttribute("name", "date")
+
+    form.appendChild(datelabel)
+    form.appendChild(dateinput)
+
+    const passdiv = document.createElement("div")
+    passdiv.setAttribute("class", "display:flex")
+
+    const passlabel = document.createElement("label")
+    passlabel.setAttribute("for", "password")
+    passlabel.innerText = "Mot de passse"
+
+    const passinput = document.createElement("input")
+    passinput.setAttribute("type", "password")
+    passinput.setAttribute("name", "password")
+    passinput.setAttribute("id", "password")
+    //passinput.setAttribute("onchange", "checkPassword()")
+
+
+    const conflabel = document.createElement("label")
+    conflabel.setAttribute("for", "password")
+    conflabel.innerText = "Confirmer mot de passse"
+
+    const confinput = document.createElement("input")
+    confinput.setAttribute("type", "password")
+    confinput.setAttribute("name", "passwordconfirm")
+    confinput.setAttribute("id", "passwordonfirm")
+    //confinput.setAttribute("onchange", "checkPassword()")
+
+    form.appendChild(passdiv)
+    passdiv.appendChild(passlabel)
+    passdiv.appendChild(passinput)
+    passdiv.appendChild(conflabel)
+    passdiv.appendChild(confinput)
+
+
+    const divradio = document.createElement("div")
+    divradio.setAttribute("style", "display:flex")
+
+    const editlabel = document.createElement("label")
+    editlabel.setAttribute("for", "edit")
+    editlabel.innerText = "Editer"
+
+    const editinput = document.createElement("input")
+    editinput.setAttribute("type", "radio")
+    editinput.setAttribute("name", "radio1")
+    editinput.setAttribute("value", "edit")
+
+    const addlabel = document.createElement("label")
+    addlabel.setAttribute("for", "edit")
+    addlabel.innerText = "Ajouter"
+
+    const addinput = document.createElement("input")
+    addinput.setAttribute("type", "radio")
+    addinput.setAttribute("name", "radio1")
+    addinput.setAttribute("value", "add")
+
+    form.appendChild(divradio)
+    divradio.appendChild(editlabel)
+    divradio.appendChild(editinput)
+    divradio.appendChild(addlabel)
+    divradio.appendChild(addinput)
+
+    const subbutton = document.createElement("button")
+    subbutton.setAttribute("onclick", "checkForm(event)")
+    subbutton.innerText = "Valider"
+
+    form.appendChild(subbutton)
+}
+monForm()
+
+// Input Radio
+for (i = 0; i < input.length; i++) {
+    input[i].onclick = function (e) {
+        if (e.ctrlKey) this.checked = false;
+    }
+}
+// Envoie du formulaire
+function checkForm(event) {
+
+    event.preventDefault()
+
+    for (let x = 0; x < input.length; x++) {
+        const element = input[x];
+        console.log(element.value)
+    }
+
+    alert("Le console.log de votre formulaire est dans la console !")
+
+    form.submit()
+}
+/*
+function checkPassword() {
+    const pass = document.getElementById('password')
+    const passConfirm = document.getElementById('passwordConfirm')
+
+    if (pass === passConfirm) {
+        console.log('Match password OK')
+        submit()
+    } else {
+        console.log('Les 2 mot de passe sont différent !!')
+        console.log(pass)
+    }
+}
+*/
